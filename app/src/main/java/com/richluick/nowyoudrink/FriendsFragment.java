5084af258722a1faeec9138183f86b767ad4246e
Rich Luick <rluick15@gmail.com>
2014-09-18 18:44:42 -0400

    Fixed Empty Inbox String

--- app/src/main/java/com/richluick/nowyoudrink/FriendsFragment.java
+++ app/src/main/java/com/richluick/nowyoudrink/FriendsFragment.java
@@ -1,6 +1,7 @@
 package com.richluick.nowyoudrink;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,7 +9,9 @@ import android.view.ViewGroup;
 /**
  * Created by Rich on 9/18/2014.
  */
-public class FriendsFragment extends android.support.v4.app.ListFragment {
+public class FriendsFragment extends Fragment {
+
+    public static final String TAG = FriendsFragment.class.getSimpleName();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
