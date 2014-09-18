777104867fe5081311dd797fbef67ba5b0cd2345
Rich Luick <rluick15@gmail.com>
2014-09-18 16:14:41 -0400

    Add Logout Ability

--- app/src/main/java/com/richluick/nowyoudrink/MainActivity.java
+++ app/src/main/java/com/richluick/nowyoudrink/MainActivity.java
@@ -84,12 +84,11 @@ public class MainActivity extends FragmentActivity implements ActionBar.TabListe
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
+
+        if (id == R.id.action_logout) {
+            ParseUser.logOut();
+            navigateToLogin();
         }
         return super.onOptionsItemSelected(item);
     }
