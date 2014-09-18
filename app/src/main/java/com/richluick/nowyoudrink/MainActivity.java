8c1907bb41bd922be188e4b5b233f5d606cbf984
Rich Luick <rluick15@gmail.com>
2014-09-18 11:54:16 -0400

    Test Commit

--- app/src/main/java/com/richluick/nowyoudrink/MainActivity.java
+++ app/src/main/java/com/richluick/nowyoudrink/MainActivity.java
@@ -1,24 +1,22 @@
 package com.richluick.nowyoudrink;
 
-import java.util.Locale;
-
-import android.app.Activity;
 import android.app.ActionBar;
+import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
-import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
+import android.support.v13.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
-import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
+import java.util.Locale;
 
+//aSAasaASasdsad
 public class MainActivity extends Activity implements ActionBar.TabListener {
 
     /**
