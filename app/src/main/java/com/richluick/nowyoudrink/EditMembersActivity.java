38a64b263ab0e0152af2473f94df566b6f494f7a
Rich Luick <rluick15@gmail.com>
2014-10-07 14:56:20 -0400

    add ability to add members to Group

--- app/src/main/java/com/richluick/nowyoudrink/EditMembersActivity.java
+++ app/src/main/java/com/richluick/nowyoudrink/EditMembersActivity.java
@@ -10,13 +10,16 @@ import android.view.View;
 import android.view.Window;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.parse.FindCallback;
+import com.parse.GetCallback;
 import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseRelation;
 import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,19 +29,21 @@ public class EditMembersActivity extends ListActivity {
 
     public static final String TAG = EditFriendsActivity.class.getSimpleName();
 
+    protected String mGroupId;
     protected MenuItem mSendMenuItem;
     protected List<ParseUser> mFriends;
     protected ArrayList<ParseUser> mPendingMembers;
     protected ParseUser mCurrentUser;
     protected ParseRelation<ParseUser> mFriendsRelation;
     protected ParseRelation<ParseUser> mPendingMemberRelation;
-    protected ParseRelation<ParseUser> mMemberRelation;
     ParseRelation<ParseObject> mMemberOfGroupRelation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_members);
+
+        mGroupId = getIntent().getStringExtra(ParseConstants.KEY_GROUP_ID);
 
         getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
@@ -51,10 +56,10 @@ public class EditMembersActivity extends ListActivity {
         mCurrentUser = ParseUser.getCurrentUser();
         mPendingMembers = new ArrayList<ParseUser>();
         mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
-        mMemberOfGroupRelation = mCurrentUser.getRelation(ParseConstants.KEY_MEMBER_OF_GROUP_RELATION);
 
         setProgressBarIndeterminateVisibility(true);
 
+        //query current users friends to populate list
         ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
         query.addAscendingOrder(ParseConstants.KEY_USERNAME);
         query.findInBackground(new FindCallback<ParseUser>() {
@@ -106,9 +111,74 @@ public class EditMembersActivity extends ListActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
         if (id == R.id.action_send) {
-            return true;
+            setProgressBarIndeterminateVisibility(true);
+            return createMemberRequest();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    //create the group member request message and send to selected recipients
+    private boolean createMemberRequest() {
+        setProgressBarIndeterminateVisibility(false);
+
+        //query the group to add relation and intent extras to request
+        ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(ParseConstants.CLASS_GROUPS);
+        query.getInBackground(mGroupId, new GetCallback<ParseObject>() {
+            @Override
+            public void done(ParseObject group, ParseException e) {
+                //add selectees to mending member relation
+                mPendingMemberRelation = group.getRelation(ParseConstants.KEY_PENDING_MEMBER_RELATION);
+                for (ParseUser member : mPendingMembers) {
+                    mPendingMemberRelation.add(member);
+                }
+                group.saveInBackground();
+
+                ParseObject message = createMessage(group);
+                if(message == null) { //error
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditMembersActivity.this);
+                    builder.setMessage(getString(R.string.error_friend_request))
+                            .setTitle(getString(R.string.error_title))
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+                else { //sends the message and goes to the new group
+                    send(message);
+                    finish();
+                }
+            }
+        });
+
+        return true;
+    }
+
+    private ParseObject createMessage(ParseObject group) {
+        ParseObject message = new ParseObject(ParseConstants.CLASS_MESSAGES);
+        message.put(ParseConstants.KEY_SENDER_ID, ParseUser.getCurrentUser().getObjectId());
+        message.put(ParseConstants.KEY_SENDER, ParseUser.getCurrentUser());
+        message.put(ParseConstants.KEY_SENDER_NAME, ParseUser.getCurrentUser().getUsername());
+        message.put(ParseConstants.KEY_RECIPIENT_IDS, mPendingMembers);
+        message.put(ParseConstants.KEY_MESSAGE_TYPE, ParseConstants.TYPE_GROUP_REQUEST);
+        message.put(ParseConstants.KEY_GROUP, group);
+        message.put(ParseConstants.KEY_GROUP_ID, "");
+
+        return message;
+    }
+
+    private void send(ParseObject message) {
+        message.saveInBackground(new SaveCallback() {
+            @Override
+            public void done(ParseException e) {
+                if(e == null) {
+                    //success
+                    Toast.makeText(EditMembersActivity.this, getString(R.string.success_group_request), Toast.LENGTH_LONG).show();
+                    //sendPushNotifications();
+                }
+                else { //error sending message
+                    Log.e(TAG, e.getMessage());
+                }
+            }
+        });
     }
 
     @Override
