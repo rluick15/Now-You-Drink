a49ba15c19ec148ee139c6fd3eef83b3c86ea317
Rich Luick <rluick15@gmail.com>
2014-10-07 13:22:38 -0400

    Add query for add members

--- app/src/main/java/com/richluick/nowyoudrink/EditMembersActivity.java
+++ app/src/main/java/com/richluick/nowyoudrink/EditMembersActivity.java
@@ -1,21 +1,95 @@
 package com.richluick.nowyoudrink;
 
-import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseRelation;
+import com.parse.ParseUser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class EditMembersActivity extends Activity {
+public class EditMembersActivity extends ListActivity {
+
+    public static final String TAG = EditFriendsActivity.class.getSimpleName();
 
     protected MenuItem mSendMenuItem;
-
+    protected List<ParseUser> mFriends;
+    protected ArrayList<ParseUser> mPendingMembers;
+    protected ParseUser mCurrentUser;
+    protected ParseRelation<ParseUser> mFriendsRelation;
+    protected ParseRelation<ParseUser> mPendingMemberRelation;
+    protected ParseRelation<ParseUser> mMemberRelation;
+    ParseRelation<ParseObject> mMemberOfGroupRelation;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_INDETERMINATE_PROGRESS);
         setContentView(R.layout.activity_edit_members);
+
+        getListView().setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        mCurrentUser = ParseUser.getCurrentUser();
+        mCurrentUser = ParseUser.getCurrentUser();
+        mPendingMembers = new ArrayList<ParseUser>();
+        mFriendsRelation = mCurrentUser.getRelation(ParseConstants.KEY_FRIENDS_RELATION);
+        mMemberOfGroupRelation = mCurrentUser.getRelation(ParseConstants.KEY_MEMBER_OF_GROUP_RELATION);
+
+        setProgressBarIndeterminateVisibility(true);
+
+        ParseQuery<ParseUser> query = mFriendsRelation.getQuery();
+        query.addAscendingOrder(ParseConstants.KEY_USERNAME);
+        query.findInBackground(new FindCallback<ParseUser>() {
+            @Override
+            public void done(List<ParseUser> parseUsers, ParseException e) {
+                setProgressBarIndeterminateVisibility(false);
+
+                if(e == null) {
+                    mFriends = parseUsers;
+                    String[] usernames = new String[mFriends.size()];
+                    int i = 0;
+                    for(ParseUser user : mFriends) {
+                        usernames[i] = user.getUsername();
+                        i++;
+                    }
+
+                    //Setup List View Adapter
+                    ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+                            EditMembersActivity.this,
+                            android.R.layout.simple_list_item_checked,
+                            usernames);
+                    setListAdapter(adapter);
+                }
+                else {
+                    Log.e(TAG, e.getMessage());
+                    AlertDialog.Builder builder = new AlertDialog.Builder(EditMembersActivity.this);
+                    builder.setTitle(R.string.error_title)
+                            .setMessage(e.getMessage())
+                            .setPositiveButton(android.R.string.ok, null);
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                }
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,5 +109,17 @@ public class EditMembersActivity extends Activity {
             return true;
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+
+        //adds the selected user to list of selected users in the list
+        if (l.isItemChecked(position)) mPendingMembers.add(mFriends.get(position));
+        else mPendingMembers.remove(mFriends.get(position)); //remove the user from the list
+
+        if(l.getCheckedItemCount() > 0) mSendMenuItem.setVisible(true);
+        else mSendMenuItem.setVisible(false);
     }
 }
